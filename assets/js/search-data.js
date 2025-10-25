aa515fcf8c7256452d8b38691109f1ce37465cdd
onsrameh <ons.rammeh@gmail.com>
2025-10-25 22:59:42 +0000

    Deploying to gh-pages from @ onsrameh/onsrameh.github.io@d67b929ad3f6cd84f167f37cabc9dd51777f1961 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
-        },{id: "dropdown-bookshelf",
+  },{id: "dropdown-bookshelf",
               title: "bookshelf",
               description: "",
               section: "Dropdown",
